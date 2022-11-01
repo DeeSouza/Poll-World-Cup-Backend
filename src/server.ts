88085b660c60f0b99bc4b7a33a0731e0654da2c4
Diego Souza <dee-souza@hotmail.com>
2022-11-01 10:02:40 -0300

    generate tabel and configure cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,34 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
+  await fastify.register(cors, {
+    origin: true,
+  });
+
+  fastify.get("/pools/count", async () => {
+    const pools = await prisma.pool.findMany({
+      where: {
+        code: {
+          startsWith: "D",
+        },
+      },
+    });
+
+    return { pools };
+  });
+
   await fastify.listen({
     port: 3333,
+    host: "0.0.0.0",
   });
 }
 
