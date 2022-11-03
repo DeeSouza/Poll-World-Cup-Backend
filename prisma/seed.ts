7f51dafd5b879d2dfe02770067e8b24122ac039c
Diego Souza <dee-souza@hotmail.com>
2022-11-03 17:04:12 -0300

    create db in prisma file

---
+++ prisma/seed.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "John Doe",
+      email: "johndoe@example.com",
+      avatarUrl: "https://github.com/deesouza.png",
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Exemplo de Bolão",
+      code: "BOL123",
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      dateAt: "2022-11-02T12:00:00.044Z",
+      firstTeamCountryCode: "DE",
+      secondTeamCountryCode: "BR",
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      dateAt: "2022-11-03T12:00:00.044Z",
+      firstTeamCountryCode: "BR",
+      secondTeamCountryCode: "AR",
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main();
